3cb6d8217f93477dc4013047839cab71ecb73772
RyanBLeon28 <ryanbleon0@gmail.com>
2024-09-09 14:39:53 -0300

    ajustes de posicionamento nas telas

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -25,103 +25,101 @@ function Home() {
 
   return (
     <>
-      <div className='body'>
-        <div className='container'>
-          <div className='barSection'>
-            <h1>LP Tarefas</h1>
-            <div>
-              <select
-                id="projectSelect"
-                className="custom-select"
-                value={selectedProject}
-                onChange={handleProjectChange}
-              >
-                <option value="" disabled hidden>Área</option>
-                <option value="faculdade">Faculdade</option>
-                <option value="trabalho">Trabalho</option>
-                <option value="casa">Casa</option>
-              </select>
-            </div>
+      <div className='container'>
+        <div className='barSection'>
+          <h1>LP Tarefas</h1>
+          <div>
+            <select
+              id="projectSelect"
+              className="custom-select"
+              value={selectedProject}
+              onChange={handleProjectChange}
+            >
+              <option value="" disabled hidden>Área</option>
+              <option value="faculdade">Faculdade</option>
+              <option value="trabalho">Trabalho</option>
+              <option value="casa">Casa</option>
+            </select>
           </div>
-          <div className='tasksArea'>
-            <div className='containerTasks'>
-              <div className='headerTasks'>
-                <h2>Tarefas</h2>
-                <button className="setting-button">
-                  <p>...</p>
-                </button>
-              </div>
-              {tasksList
-                .filter(task =>
-                  task.title.includes(searchTerm) &&
-                  (!selectedProject || task.area === selectedProject) &&
-                  task.status === 'toDo'
-                )
-                .map((task) => (
-                  <div key={task.id} className="task-item">
-                    <h4>{task.title}</h4>
-                    <p><strong>Responsável:</strong> {task.responsible}</p>
-                    <p><strong>Tarefa:</strong> {task.task}</p>
-                    <button className="status-button" onClick={() => updateTaskStatus(task.id, 'doing')}>
-                      Iniciar
-                    </button>
-                  </div>
-              ))}
-              <button className="add-button">
-                <ModalAddTask />
+        </div>
+        <div className='tasksArea'>
+          <div className='containerTasks'>
+            <div className='headerTasks'>
+              <h2>Tarefas</h2>
+              <button className="setting-button">
+                <p>...</p>
               </button>
             </div>
-            <div className='containerTasks'>
-              <div className='headerTasks'>
-                <h2>Em Andamento</h2>
-                <button className="setting-button">
-                  <p>...</p>
-                </button>
-              </div>
-              {tasksList
-                .filter(task =>
-                  task.title.includes(searchTerm) &&
-                  (!selectedProject || task.area === selectedProject) &&
-                  task.status === 'doing'
-                )
-                .map((task) => (
-                  <div key={task.id} className="task-item">
-                    <h4>{task.title}</h4>
-                    <p><strong>Responsável:</strong> {task.responsible}</p>
-                    <p><strong>Tarefa:</strong> {task.task}</p>
-                    <button className="status-button" onClick={() => updateTaskStatus(task.id, 'toDo')}>
-                      Reverter
-                    </button>
-                    <button className="status-button" onClick={() => updateTaskStatus(task.id, 'done')}>
-                      Concluir
-                    </button>
-                  </div>
-              ))}
+            {tasksList
+              .filter(task =>
+                task.title.includes(searchTerm) &&
+                (!selectedProject || task.area === selectedProject) &&
+                task.status === 'toDo'
+              )
+              .map((task) => (
+                <div key={task.id} className="task-item">
+                  <h4>{task.title}</h4>
+                  <p><strong>Responsável:</strong> {task.responsible}</p>
+                  <p><strong>Tarefa:</strong> {task.task}</p>
+                  <button className="status-button" onClick={() => updateTaskStatus(task.id, 'doing')}>
+                    Iniciar
+                  </button>
+                </div>
+            ))}
+            <button className="add-button">
+              <ModalAddTask />
+            </button>
+          </div>
+          <div className='containerTasks'>
+            <div className='headerTasks'>
+              <h2>Em Andamento</h2>
+              <button className="setting-button">
+                <p>...</p>
+              </button>
             </div>
-            <div className='containerTasks'>
-              <div className='headerTasks'>
-                <h2>Concluídas</h2>
-                <button className="setting-button">
-                  <p>...</p>
-                </button>
-              </div>
-              {tasksList
-                .filter(task =>
-                  task.title.includes(searchTerm) &&
-                  (!selectedProject || task.area === selectedProject) &&
-                  task.status === 'done'
-                )
-                .map((task) => (
-                  <div key={task.id} className="task-item">
-                    <h4>{task.title}</h4>
-                    <p><strong>Responsável:</strong> {task.responsible}</p>
-                    <p><strong>Tarefa:</strong> {task.task}</p>
-                    <button className="status-button" onClick={() => updateTaskStatus(task.id, 'doing')}>
-                      Reabrir
-                    </button>
-                  </div>
-              ))}
+            {tasksList
+              .filter(task =>
+                task.title.includes(searchTerm) &&
+                (!selectedProject || task.area === selectedProject) &&
+                task.status === 'doing'
+              )
+              .map((task) => (
+                <div key={task.id} className="task-item">
+                  <h4>{task.title}</h4>
+                  <p><strong>Responsável:</strong> {task.responsible}</p>
+                  <p><strong>Tarefa:</strong> {task.task}</p>
+                  <button className="status-button" onClick={() => updateTaskStatus(task.id, 'toDo')}>
+                    Reverter
+                  </button>
+                  <button className="status-button" onClick={() => updateTaskStatus(task.id, 'done')}>
+                    Concluir
+                  </button>
+                </div>
+            ))}
+          </div>
+          <div className='containerTasks'>
+            <div className='headerTasks'>
+              <h2>Concluídas</h2>
+              <button className="setting-button">
+                <p>...</p>
+              </button>
             </div>
+            {tasksList
+              .filter(task =>
+                task.title.includes(searchTerm) &&
+                (!selectedProject || task.area === selectedProject) &&
+                task.status === 'done'
+              )
+              .map((task) => (
+                <div key={task.id} className="task-item">
+                  <h4>{task.title}</h4>
+                  <p><strong>Responsável:</strong> {task.responsible}</p>
+                  <p><strong>Tarefa:</strong> {task.task}</p>
+                  <button className="status-button" onClick={() => updateTaskStatus(task.id, 'doing')}>
+                    Reabrir
+                  </button>
+                </div>
+            ))}
           </div>
         </div>
       </div>
