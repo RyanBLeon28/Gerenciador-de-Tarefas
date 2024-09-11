4712ebc6a2e70b08f2159b662bd9d2f520101b07
RyanBLeon28 <ryanbleon0@gmail.com>
2024-09-10 23:19:32 -0300

    mudancas

--- frontend/src/context/index.jsx
+++ frontend/src/context/index.jsx
@@ -8,6 +8,7 @@ const TasksProvider = ({ children }) => {
 
     const [selectedProject, setSelectedProject] = useState("");
     const [searchTerm, setSearchTerm] = useState("");
+    const [error, setError] = useState('');
 
     const [tasksList, setTasksList] = useState(initialTasksList);
 
@@ -16,10 +17,39 @@ const TasksProvider = ({ children }) => {
     const [ done, setDone ] = useState([]);
 
     useEffect(() => {
-        setToDo(tasksList.filter(task => task.status === "toDo"));
-        setDoing(tasksList.filter(task => task.status === "doing"));
-        setDone(tasksList.filter(task => task.status === "done"));
+        const fetchTasks = async () => {
+            try {
+                const response = await fetch('http://localhost:6900/user/tasklist', {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ username, password }), // Verifique se username e password estão disponíveis
+                });
+
+                if (!response.ok) {
+                    throw new Error('Erro ao buscar os dados');
+                }
+
+                const data = await response.json();
+                console.log("Resposta",data)
+                // Filtra as tarefas com base no status
+                // setToDo(data.filter(task => task.status === 'toDo'));
+                // setDoing(data.filter(task => task.status === 'doing'));
+                // setDone(data.filter(task => task.status === 'done'));
+            } catch (err) {
+                setError(err.message);
+            }
+        };
+
+        fetchTasks();
     }, []);
+
+    // useEffect(() => {
+    //     setToDo(tasksList.filter(task => task.status === "toDo"));
+    //     setDoing(tasksList.filter(task => task.status === "doing"));
+    //     setDone(tasksList.filter(task => task.status === "done"));
+    // }, []);
 
     useEffect(() => {
         let filteredTasks = tasksList
